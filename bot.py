98cdbc415cdd1fda17ec55fa091fb98b3a9b0e61
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2025-06-18 12:29:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import logging
 import logging.config
 import warnings
-from pyrogram import Client, idle
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from config import Config
@@ -12,6 +11,8 @@ import asyncio
 from plugins.web_support import web_server
 import pyromod
 
+
+# Configure logging
 logging.config.fileConfig("logging.conf")
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
@@ -34,20 +35,22 @@ class Bot(Client):
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username
+
+        # Start web server
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, Config.PORT).start()
+        await web.TCPSite(app, "0.0.0.0", Config.PORT).start()
+
         logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
 
+        # Notify admins
         for id in Config.ADMIN:
             try:
-                await self.send_message(
-                    id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**"
-                )
+                await self.send_message(id, f"**__{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️__**")
             except:
                 pass
 
+        # Log channel message
         if Config.LOG_CHANNEL:
             try:
                 curr = datetime.now(timezone("Asia/Kolkata"))
@@ -55,10 +58,10 @@ class Bot(Client):
                 time = curr.strftime("%I:%M:%S %p")
                 await self.send_message(
                     Config.LOG_CHANNEL,
-                    f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n\🤖 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`</b>",
+                    f"**__{me.mention} Iꜱ Rᴇsᴛᴀʀᴛᴇᴅ !!**\n\n📅 Dᴀᴛᴇ : `{date}`\n⏰ Tɪᴍᴇ : `{time}`\n🌐 Tɪᴍᴇᴢᴏɴᴇ : `Asia/Kolkata`\n🤖 Vᴇʀsɪᴏɴ : `v{__version__} (Layer {layer})`",
                 )
             except:
-                print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
+                print("Make sure the bot is admin in the log channel.")
 
     async def stop(self, *args):
         await super().stop()
@@ -70,13 +73,7 @@ bot_instance = Bot()
 
 def main():
     async def start_services():
-        if Config.STRING_SESSION:
-            await asyncio.gather(
-                app.start(),  # Start the Pyrogram Client
-                bot_instance.start(),  # Start the bot instance
-            )
-        else:
-            await asyncio.gather(bot_instance.start())
+        await bot_instance.start()
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_services())
